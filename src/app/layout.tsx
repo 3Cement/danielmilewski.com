56de446930a85983a629ecf873b250738f28c8ee
Daniel <danielmilewski123@gmail.com>
2026-04-02 22:46:44 +0200

    fix(i18n): avoid incorrect root html lang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,7 +62,6 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
       suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable}`}
     >
